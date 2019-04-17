7cb69b62af32c671f354ce7eed6944cf50c565d4
zexUlt <danil.chernozubov66@gmail.com>
2019-04-17 09:30:18 +0500

    Tree and Set v1.0

---
+++ Set.cpp
@@ -0,0 +1,71 @@
+//
+// Created by zexult on 28.03.19.
+//
+
+
+#include "Set.h"
+
+int Set::insert(int elem, size_t _size)
+{
+    if(tree.find(elem))
+        return 1;
+    else {
+        this->tree.insertVal(elem);
+        return 0;
+    }
+}
+
+Set::Set(int)
+{
+
+}
+
+int Set::size()
+{
+
+}
+
+size_t Set::max_bytes()
+{
+
+}
+
+RBTree::SetIterator* Set::begin()
+{
+    RBTree::SetIterator *it;
+    Node* begin = this->tree.minimalNode(this->tree.getRoot());
+    it->setCurrent(begin);
+    return it;
+}
+
+RBTree::SetIterator * Set::end()
+{
+    RBTree::SetIterator *it;
+    it->setCurrent(it->getEnd());
+    return it;
+}
+
+RBTree::SetIterator * Set::find(void *elem, size_t size)
+{
+
+}
+
+void Set::clear()
+{
+
+}
+
+RBTree::SetIterator * Set::newIterator()
+{
+
+}
+
+void Set::remove(RBTree::SetIterator *iter)
+{
+
+}
+
+bool Set::empty()
+{
+
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by zexult on 03.04.19.
+//
+
+#include "Set.h"
+
+int main()
+{
+    return 0;
+}
