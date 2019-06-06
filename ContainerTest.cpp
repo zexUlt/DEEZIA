1a6109e6dba5c2471d81b48d6f1f2ddc1ee4344a
zexUlt <danil.chernozubov66@gmail.com>
2019-06-06 12:54:25 +0500

    Class generating tests for your container

--- ContainerTest.cpp
+++ ContainerTest.cpp
@@ -3,22 +3,108 @@
 //
 
 #include "ContainerTest.h"
+#include "RBTree.h"
+#include <stdlib.h>
+#include <random>
+#include <algorithm>
+#include <typeinfo>
 
 ContainerTest::ContainerTest(Set* _container)
 {
     cont = _container;
 }
 
-double ContainerTest::testInsert(size_t size)
+char* ContainerTest::rand_string(size_t len)
 {
-    clock_t start = clock();
-    for(int i = 0; i < size; i++)
-    {
-        this->cont->insert(&i, sizeof(i));
-    }
-    clock_t finish = clock();
+    char* rand_str = new char[len];
 
-    return (double) (finish - start) / CLOCKS_PER_SEC;
+    const char ch_set[] = {'A','B','C','D','E','F',
+                          'G','H','I','J','K',
+                          'L','M','N','O','P',
+                          'Q','R','S','T','U',
+                          'V','W','X','Y','Z',
+                          'a','b','c','d','e','f',
+                          'g','h','i','j','k',
+                          'l','m','n','o','p',
+                          'q','r','s','t','u',
+                          'v','w','x','y','z'
+                         };
+
+    std::default_random_engine rng(std::random_device{}());
+
+    std::uniform_int_distribution<> dist(0, (int)strlen(ch_set)-1);
+
+    auto rand_char = [ch_set, &dist, &rng](){return ch_set[ dist(rng) ]; };
+
+    generate_n(rand_str, len, rand_char);
+
+    return rand_str;
+}
+
+double ContainerTest::testInsert(size_t size, int flag)
+{
+    clock_t time(0), start, finish;
+    switch (flag)
+    {
+        case CT_NORMAL:
+            start = clock();
+            for(int i = 0; i < size; i++)
+            {
+                this->cont->insert(&i, sizeof(i));
+                this->cont->name = typeid(i).name();
+            }
+            finish = clock();
+            time = finish - start;
+            break;
+
+        case CT_EQUAL:
+            start = clock();
+            for(int i = 0; i < size; i++)
+                for(int j = 0; j < sqrt(size)/2; j++)
+                {
+                    this->cont->insert(&i, sizeof(i));
+                    this->cont->name = typeid(i).name();
+                }
+            finish = clock();
+            time = finish - start;
+            break;
+
+        case CT_STRINT:
+            start = clock();
+            for(int i = 0; i < size; i++)
+            {
+                if (i % 2) {
+                    char* str = rand_string((size_t) i);
+                    this->cont->name = typeid(str).name();
+                    this->cont->insert(str, (size_t) i);
+                } else {
+                    this->cont->name = typeid(i).name();
+                    this->cont->insert(&i, sizeof(i));
+                }
+            }
+            finish = clock();
+            time = finish - start;
+            break;
+
+        case CT_CHRINT:
+            start = clock();
+            for(int i = 0; i < size; i++)
+            {
+                if (i % 2) {
+                    char* ch = rand_string(1);
+                    this->cont->name = typeid(ch).name();
+                    this->cont->insert(ch, sizeof(ch));
+                } else {
+                    this->cont->name = typeid(i).name();
+                    this->cont->insert(&i, sizeof(i));
+                }
+            }
+            finish = clock();
+            time = finish - start;
+            break;
+    }
+
+    return (double) time / CLOCKS_PER_SEC;
 }
 
 double ContainerTest::testRemove(size_t n)
@@ -27,7 +113,7 @@ double ContainerTest::testRemove(size_t n)
     Container::Iterator *it = cont->newIterator();
 
     if (n >= this->cont->size())
-        time = testFullremove();
+        time = testClear();
     else {
         clock_t start = clock();
 
@@ -48,7 +134,7 @@ double ContainerTest::testRemove(size_t n)
     return time;
 }
 
-double ContainerTest::testFullremove()
+double ContainerTest::testClear()
 {
     clock_t start = clock();
 
@@ -62,11 +148,21 @@ double ContainerTest::testFullremove()
 void ContainerTest::printContainer()
 {
     Container::Iterator* it = cont->newIterator();
+    auto s_it = (RBTree::SetIterator*) it;
     size_t size;
 
-    for(it = cont->begin(); !it->equals(cont->end()); it->goToNext())
+    for(it = this->cont->begin(); !it->equals(this->cont->end()); it->goToNext())
     {
-        cout << *(int*)it->getElement(size) << " ";
+        s_it = (RBTree::SetIterator*)it;
+        if(s_it->current->name == "i") {
+            int elem = *(int *) it->getElement(size);
+            printf("%20d | size (bytes) = %lu\n", elem, size);
+        }
+       if(s_it->current->name == "Pc"){
+           char* elem = (char*)it->getElement(size);
+           printf("%20s | size (bytes) = %lu\n", elem, size);
+       }
+
     }
     cout << endl;
 }
--- ContainerTest.h
+++ ContainerTest.h
@@ -5,20 +5,27 @@
 #ifndef DEEZIA_CONTAINERTEST_H
 #define DEEZIA_CONTAINERTEST_H
 
-
 #include "GroupContainer.h"
 #include "Set.h"
 #include <ctime>
 
+#define CT_NORMAL 1
+#define CT_EQUAL 2
+#define CT_STRINT 3
+#define CT_CHRINT 4
 
 
 class ContainerTest {
 private:
     Set *cont;
+
+    char* rand_string(size_t);
+
 public:
-    explicit ContainerTest(Set*);
-    double testFullremove(); // is working
-    double testInsert(size_t); // is working
+    explicit ContainerTest(Set *);
+
+    double testClear(); // is working
+    double testInsert(size_t, int); // is working
     double testRemove(size_t); // need fix
     void printContainer(); // is working
 };
